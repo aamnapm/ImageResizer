97f437ac80d1ded6d25f9270c849955d5c452fa3
ali <aliakbari76318@gmail.com>
2020-11-20 19:58:04 +0330

    create project

---
+++ src/test/java/ir/aamnapm/image_resizer/ImageResizerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.aamnapm.image_resizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageResizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
