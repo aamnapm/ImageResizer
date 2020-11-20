9b0ad0684d54c8f4e5cd3a73276267967cb7739b
ali <aliakbari76318@gmail.com>
2020-11-20 21:32:21 +0330

    create UploadRestController

---
+++ src/main/java/ir/aamnapm/resize_image/controller/UploadRestController.java
@@ -0,0 +1,8 @@
+package ir.aamnapm.resize_image.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/upload")
+public class UploadRestController {
+
+}
