5350ef4316916c95ef81123dc78fb9a29abfafa7
ali <aliakbari76318@gmail.com>
2020-11-22 21:54:13 +0330

    add methods(uploadMultipartFile , uploadBase64File)

--- src/main/java/ir/aamnapm/resize_image/controller/UploadRestController.java
+++ src/main/java/ir/aamnapm/resize_image/controller/UploadRestController.java
@@ -1,8 +1,35 @@
 package ir.aamnapm.resize_image.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import ir.aamnapm.resize_image.dto.FileUploadDTO;
+import ir.aamnapm.resize_image.iservice.IFileService;
+import lombok.AllArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-@RestController(value = "/upload")
+import javax.validation.constraints.Pattern;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/upload")
+@Validated
 public class UploadRestController {
+
+    private final IFileService iFileService;
+
+    @PostMapping(value = "/multipartFile")
+    public ResponseEntity<ByteArrayResource> uploadMultipartFile(
+            @RequestPart("file") MultipartFile file,
+            @Pattern(regexp = "image\\/resize((,m_(?<method>\\w+))*(,l_(?<longer>\\d+))*(,w_(?<width>\\d+))*(,h_(?<height>\\d+))*){4}"
+                    , message = "structure not true") String process) {
+        return iFileService.multipartFileResize(file, process);
+    }
+
+    @PostMapping(value = "/base64")
+    public void uploadBase64File(@RequestBody FileUploadDTO fileUploadDTO) {
+        iFileService.base64Resize(fileUploadDTO);
+    }
 
 }
