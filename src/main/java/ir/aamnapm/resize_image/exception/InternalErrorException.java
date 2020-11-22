a19f0b79d37bcc02c1c896ba347eb0650e682d41
ali <aliakbari76318@gmail.com>
2020-11-22 21:57:07 +0330

    create FileUploadDTO InternalErrorException

---
+++ src/main/java/ir/aamnapm/resize_image/exception/InternalErrorException.java
@@ -0,0 +1,8 @@
+package ir.aamnapm.resize_image.exception;
+
+public class InternalErrorException extends RuntimeException {
+
+    public InternalErrorException(String message) {
+            super(message);
+        }
+}
